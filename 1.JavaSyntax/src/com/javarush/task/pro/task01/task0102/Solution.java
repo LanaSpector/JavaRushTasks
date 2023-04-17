9c64a0973d16d7b91406c99f67dc6e7d7e10fdbe
LanaSpector <mansurova.s.k@gmail.com>
2023-04-17 23:04:49 +0500

    Add solution for java syntax task0102

--- 1.JavaSyntax/src/com/javarush/task/pro/task01/task0102/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0102/Solution.java
@@ -7,6 +7,7 @@ package com.javarush.task.pro.task01.task0102;
 public class Solution {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println("Аварийное питание включено");
+        System.out.print("Ручное управление включено");
     }
 }
