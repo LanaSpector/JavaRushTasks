7a37f8dd3698f653905518063d7cd1c0e7e41578
LanaSpector <mansurova.s.k@gmail.com>
2023-04-20 22:04:52 +0500

    Add solution for java syntax task04

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task04.task0408;
+
+import java.util.Scanner;
+
+/* 
+Максимум из введенных чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+
+        while (sc.hasNextInt()) {
+            int number = sc.nextInt();
+            if (number > max && number%2==0) {
+                max = number;
+            }
+        }
+        System.out.println(max);
+
+    }
+}
