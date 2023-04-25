95ec3104478d7bde02dc5274555755b1a45035c5
LanaSpector <mansurova.s.k@gmail.com>
2023-04-25 11:41:09 +0500

    Add solution for java syntax task0529(2) "создание переменных класса, двумерного массива и его заполнение. Изменение одного значения в каждом ряду массива"

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
@@ -12,14 +12,19 @@ public class Solution {
     public static String hit = "🎯";
     public static int width = 30;
     public static int height = 10;
-    public static String [][] field = new String[height][width];
+    public static String[][] field = new String[height][width];
 
 
     public static void main(String[] args) {
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
-                field[i][j] = empty;
-                System.out.print(field[i][j]);
+                if (j == 0) {
+                    field[i][j] = robotank;
+                    System.out.print(field[i][j]);
+                } else {
+                    field[i][j] = empty;
+                    System.out.print(field[i][j]);
+                }
             }
             System.out.println();
         }
