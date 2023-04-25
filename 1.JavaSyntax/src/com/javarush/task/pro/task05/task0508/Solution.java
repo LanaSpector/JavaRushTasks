61ab7e883df9a5a4f931e5d51dbe257d5be1512b
LanaSpector <mansurova.s.k@gmail.com>
2023-04-25 08:36:08 +0500

    Add solution for java syntax task0508

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -10,7 +10,18 @@ public class Solution {
     public static String[] strings;
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        strings = new String [6];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = sc.nextLine();
+        }
+        int tmp;
+        for (int i = 0; i < strings.length; i++) {
+
+
+
+        }
+
         for (int i = 0; i < strings.length; i++) {
             System.out.print(strings[i] + ", ");
         }
