78212aa9b0d6c683dc2cdb1fd6f436ec7c9e6649
LanaSpector <mansurova.s.k@gmail.com>
2023-04-27 23:14:15 +0500

    Add solution for java syntax task0608

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task06.task0608;
+
+/* 
+Кубический калькулятор
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long x) {
+        return x*x*x;
+    }
+}
