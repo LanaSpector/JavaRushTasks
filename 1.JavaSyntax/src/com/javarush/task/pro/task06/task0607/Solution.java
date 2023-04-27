909e1458ef92739c4ffc2952448fffb045b14b51
LanaSpector <mansurova.s.k@gmail.com>
2023-04-27 23:13:49 +0500

    Add solution for java syntax task0607

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0607/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task06.task0607;
+
+/* 
+Добро пожаловать! Но не всем.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void signIn(String username){
+        if (username.equals("user")) {
+            return;
+        } else {
+        System.out.println("Добро пожаловать " + username);
+        System.out.println("Очень скучали по Вам, " + username);
+        }
+    }
+}
