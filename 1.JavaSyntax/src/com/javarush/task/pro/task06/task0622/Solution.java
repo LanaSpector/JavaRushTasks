0551312e0af82a410b7f304d5aa31852c4582ef2
LanaSpector <mansurova.s.k@gmail.com>
2023-04-29 23:27:41 +0500

    Add solution for java syntax task0622

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
@@ -15,10 +15,19 @@ public class Solution {
     public static String winPhrase = "Амиго одержал победу над Диабло и выбил из его головы загадочный кристалл.";
     public static String loosePhrase = "Диабло победил Амиго.";
     public static int diabloPosition;
+    public static int amigoLives = 9;
+    public static int diabloLives = 9;
 
     public static void main(String[] args) {
         diabloPosition = getRandomNumber(4);
         findDiablo();
+        battle();
+        isAmigoWin();
+        if(isAmigoWin()==true) {
+            System.out.print(winPhrase);
+        } else {
+            System.out.print(loosePhrase);
+        }
     }
 
     public static int getRandomNumber(int range) {
@@ -38,6 +47,47 @@ public class Solution {
             else {
                 System.out.print(getPositionPhrase);
             }
+        }
+    }
+    
+    public static void amigoLostLife() {
+        -- amigoLives;
+    }
+    
+    public static void diabloLostLife() {
+        diabloLives -= 3;
+    }
+    
+    public static int amigoAttacks() {
+        return getRandomNumber(3);
+    }
+        
+    public static int diabloDefends() {
+        return getRandomNumber(3);
+    }
+    
+    public static void battle() {
+        while(amigoLives>0 || diabloLives>0) {
+            if(amigoLives<=0 || diabloLives<=0) {
+                break;
+            }
+            amigoAttacks();
+            if(amigoAttacks()!=diabloDefends()) {
+                System.out.print(amigoAttackPhrase);
+                diabloLostLife();
+            }
+            else {
+                System.out.print(diabloDefendPhrase);
+                amigoLostLife();
+            }
+        } 
+    }
+    
+    public static boolean isAmigoWin() {
+        if(diabloLives<=0) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
