82355fe954aad1a92ff94b78c284006259890892
LanaSpector <mansurova.s.k@gmail.com>
2023-04-29 23:32:23 +0500

    Add solution for java syntax task0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte b;
+    short s;
+    int n;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bo;
+}
