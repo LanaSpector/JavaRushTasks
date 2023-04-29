0ab627cf59b308b1591a3e8e5f5daef6c19f04ea
LanaSpector <mansurova.s.k@gmail.com>
2023-04-29 23:35:49 +0500

    Add solution for java syntax task0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    char c = 65535;
+    byte b = 127;
+    short s = 32767;
+    int i = Integer.MAX_VALUE;
+    long l = 9223372036854775807L;
+    float f = (float)(3.14 * Math.pow(10, 38));
+    double d = (1.7 * Math.pow(10, 308));
+    boolean bo = false;
+    
+}
