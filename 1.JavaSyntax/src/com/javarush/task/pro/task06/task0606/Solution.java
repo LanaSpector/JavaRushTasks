fabec23c44c6df549a208e421ba30879d6acf210
LanaSpector <mansurova.s.k@gmail.com>
2023-04-29 16:06:56 +0500

    Add solution for java syntax task0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(String g){
+
+    }
+
+    public static void universalMethod(String f, String r){
+
+    }
+
+    public static void universalMethod(int r){
+
+    }
+    public static void universalMethod(int e, String d){
+
+    }
+
+    public static void universalMethod(int w, int r){
+
+    }
+
+    public static void universalMethod(double r){
+
+    }
+
+    public static void universalMethod(int y, double t){
+
+    }
+
+    public static void universalMethod(float q){
+
+    }
+
+    public static void universalMethod(char s){
+
+    }
+
+    //напишите тут ваш код
+}
