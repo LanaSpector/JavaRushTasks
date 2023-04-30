8d991e67ea5a76b6f6d0d4b755267ece1b07e8ac
LanaSpector <mansurova.s.k@gmail.com>
2023-04-30 09:56:28 +0500

    Add solution for java syntax task0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    char c = 0;
+    byte b = -128;
+    short s = -32768;
+    int i = -2147483648;
+    long l = -9223372036854775808L;
+    float f = (float)(-3.14 * Math.pow(10, 38));
+    double d = 4.9 * Math.pow(10, -324);
+    boolean bo = false;
+
+}
