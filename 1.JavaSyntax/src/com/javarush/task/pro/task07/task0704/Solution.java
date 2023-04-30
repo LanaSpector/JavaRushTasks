e8e393272684d03555abb7508bf5f96ee4be8b9f
LanaSpector <mansurova.s.k@gmail.com>
2023-04-30 10:33:23 +0500

    Add solution for java syntax task0704

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0704/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0704;
+
+/* 
+Экспонентная запись числа
+*/
+
+public class Solution {
+    double earthDiameter = 1.27420e+4;
+    double lightSpeed = 2.997924580e+8;
+    double uraniumAtomicMass = 2.380289e+2;
+    double averageBeeWeight = 008.5e-2;
+    double javaDeveloperSalary = 1.00000e+4;
+}
