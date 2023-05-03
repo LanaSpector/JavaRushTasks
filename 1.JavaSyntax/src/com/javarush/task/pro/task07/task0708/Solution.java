06320f61c2b4e4f061ae22e7e8b6a4e5451a082d
LanaSpector <mansurova.s.k@gmail.com>
2023-05-03 18:03:09 +0500

    Add solution for java syntax task0715

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0708/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task07.task0708;
+
+/* 
+Правильные типы
+*/
+
+public class Solution {
+    public static byte a;
+    public static short b;
+    public static int c;
+    public static long d;
+
+    public static void main(String[] args) {
+        setValues(100);
+    }
+
+    public static void setValues(long value){
+        //напишите тут ваш код
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0715/Human.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task07.task0715;
+
+public class Human extends Entity{
+
+
+    public void speak(){
+        System.out.println("Я умею общаться.");
+    }
+}
