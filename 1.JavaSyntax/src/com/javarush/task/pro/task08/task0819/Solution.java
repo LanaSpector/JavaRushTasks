faa8c295bf45dc1144ca55f2ac3e77bafcef08f8
LanaSpector <mansurova.s.k@gmail.com>
2023-05-03 19:26:13 +0500

    Add solution for java syntax task0819

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0819/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task08.task0819;
+
+import java.util.Random;
+
+/* 
+Амиголятор и Био-диего
+*/
+
+import static com.javarush.task.pro.task08.task0819.CosmicAnomaly.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        for (int i = 0; i < 30; i++) {
+            int number = random.nextInt (upperBound - lowerBound)+lowerBound;
+            int result = nextAttempt(number);
+            if(result==number) {
+                break;
+            }
+        }
+    }
+}
