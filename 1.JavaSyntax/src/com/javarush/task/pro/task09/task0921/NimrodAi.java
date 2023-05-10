2b039ab6f1694718956a9c72ae567ceeae210818
LanaSpector <mansurova.s.k@gmail.com>
2023-05-10 20:03:54 +0500

    Add solution for java syntax task0921

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0921/NimrodAi.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task09.task0921;
+
+/* 
+Пиратский разворот
+*/
+
+import java.util.Arrays;
+
+public class NimrodAi {
+    
+    public static void main(String[] args) {
+        PastryLoader[] pastryLoaders = new PastryLoader[]{new PastryLoader(), new PastryLoader(), new PastryLoader()};
+
+        scanMemory(pastryLoaders);
+        replaceTarget(pastryLoaders);
+        scanMemory(pastryLoaders);
+    }
+
+    public static void scanMemory(PastryLoader[] pastryLoaders) {
+        System.out.println();
+        for (PastryLoader pastryLoader : pastryLoaders) {
+            System.out.println(Arrays.deepToString(pastryLoader.memory));
+        }
+    }
+
+    public static void replaceTarget(PastryLoader[] pastryLoaders) {
+        for (PastryLoader pastryLoader : pastryLoaders) {
+            String[][][] array = pastryLoader.memory;
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    for (int k = 0; k < array[i][j].length; k++) {
+                        String string = array[i][j][k];
+                        String str = string.toLowerCase();
+                        if(str.contains("nimrod")) {
+                            array[i][j][k] = str.replaceAll("nimrod", "pirate ship");
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
