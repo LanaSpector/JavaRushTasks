1eac4e3dc9addec92bb9d1a6fe72ab03f6e30222
LanaSpector <mansurova.s.k@gmail.com>
2023-05-11 21:32:22 +0500

    Add solution for java syntax task0918

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0919/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task09.task0919;
+
+/* 
+Разворот строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Ходит кот задом наперед";
+        System.out.println(string);
+        System.out.println(reverseString(string));
+    }
+
+    public static String reverseString(String string) {
+        StringBuilder sb = new StringBuilder(string);
+        sb.reverse();
+        String str = sb.toString();
+        return str;
+    }
+}
