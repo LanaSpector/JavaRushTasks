666f0c8cb33dd6f0f54fd398a59a927fa6c7a48e
LanaSpector <mansurova.s.k@gmail.com>
2023-05-16 14:53:59 +0500

    Add solution for java syntax task1017

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        Africa africa = new Africa(30_300_000);
+        Antarctica antarctica = new Antarctica(14_100_000);
+        Australia australia = new Australia(7_700_000);
+        Eurasia eurasia = new Eurasia(54_600_000);
+        NorthAmerica northAmerica = new NorthAmerica(24_400_000);
+        SouthAmerica southAmerica = new SouthAmerica(17_800_000);
+
+    }
+}
