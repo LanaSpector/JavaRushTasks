47df82923bba2f8bf85e2883096b5461499ae8fb
LanaSpector <mansurova.s.k@gmail.com>
2023-05-19 18:29:18 +0500

    Add solution for java syntax task1118

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1118/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task11.task1118;
+import java.util.Scanner;
+/* 
+Импортный код
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
