a46d0b34161f974f712b1e470f9ccfc5b8fafc92
LanaSpector <mansurova.s.k@gmail.com>
2023-05-26 20:45:10 +0500

    Add solution for java syntax task1333

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1333/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.pro.task13.task1333;
+
+/* 
+Спасти рядового Компиляена
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static List<Integer> ratingsList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        setRatings();
+        printList(); // before
+        improveRatings();
+        printList(); // after
+    }
+
+    public static void changeNegativeNumbers() {
+        for(int i=0; i<ratingsList.size(); i++) {
+        Integer tmp = ratingsList.get(i);
+        if(tmp<0) {
+            tmp *= -1;
+            ratingsList.set(i, tmp);
+        }
+        }
+    }
+
+    public static void changeZerosToMaximumNumber() {
+        Integer max = Collections.max(ratingsList);
+        Collections.replaceAll(ratingsList, 0, max);
+    }
+
+    public static void reverseList() {
+        Collections.sort(ratingsList);
+        Collections.reverse(ratingsList);
+    }
+
+    public static void improveRatings() {
+        changeNegativeNumbers();
+        changeZerosToMaximumNumber();
+        reverseList();
+    }
+
+    private static void setRatings() {
+        int rating;
+        Random random = new Random();
+        int min = -5;
+        int max = 11;
+
+        for (int i = 0; i < 20; i++) {
+            rating = random.nextInt(max - min) + min;
+            ratingsList.add(rating);
+        }
+    }
+
+    private static void printList() {
+        ratingsList.forEach((value) -> System.out.print(value + " "));
+        System.out.println();
+    }
+}
