d05952b120bf448f1f0f07a99ae951afcde123a3
LanaSpector <mansurova.s.k@gmail.com>
2023-05-30 19:09:49 +0500

    Add solution for java syntax task1318

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1318/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task13.task1318;
+
+/* 
+Следующий месяц, пожалуйста
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getNextMonth(Month.JANUARY));
+        System.out.println(getNextMonth(Month.JULY));
+    }
+
+    public static Month getNextMonth(Month month) {
+        int index = month.ordinal();
+        return index==11? Month.values()[0] : Month.values()[index+1];
+
+    
+    }
+}
