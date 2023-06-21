3eda9e75918bbe497b4b8c052f1337f10d43ab3d
LanaSpector <mansurova.s.k@gmail.com>
2023-06-21 19:54:25 +0500

    Add solution for java syntax task1515

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1515/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task15.task1515;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Абсолютный путь
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Path path = Path.of(str);
+        System.out.println(path.isAbsolute() ? path : path.toAbsolutePath());
+    }
+}
+
