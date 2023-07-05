98d44cfd72f11b2f770c45e5f3fe702f0c09fbb5
LanaSpector <mansurova.s.k@gmail.com>
2023-07-05 23:41:04 +0500

    Add solution for java syntax task1702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1702/Circle.java
@@ -0,0 +1,6 @@
+package com.javarush.task.pro.task17.task1702;
+
+public class Circle extends Shape {
+
+    public void printInfo() {System.out.println("Круг");}
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1702/Triangle.java
@@ -0,0 +1,5 @@
+package com.javarush.task.pro.task17.task1702;
+
+public class Triangle extends Shape {
+    public void printInfo() {System.out.println("Треугольник");}
+}
