08c4eaa09c85a067109720fc2da8b7a7f9aeb946
LanaSpector <mansurova.s.k@gmail.com>
2023-07-08 12:50:59 +0500

    Add solution for java syntax task1716

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1716/Vehicle.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task17.task1716;
+
+/* 
+Дорожное движение
+*/
+
+public interface Vehicle {
+    default void start() {}
+    void move();
+    default void stop() {}
+}
