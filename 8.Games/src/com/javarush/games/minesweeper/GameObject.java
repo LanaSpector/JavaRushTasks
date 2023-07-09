6cd589841a3a37a4129aa01cc57c2a2b3c493fd1
LanaSpector <mansurova.s.k@gmail.com>
2023-07-09 14:33:21 +0500

    Add solution for MinesweeperGame, part 4 of 16

--- 8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -4,7 +4,7 @@ public class GameObject {
     public int x;
     public int y;
 
-    public GameObject(int x, int y){
+    GameObject(int x, int y){
         this.x = x;
         this.y = y;
     }
