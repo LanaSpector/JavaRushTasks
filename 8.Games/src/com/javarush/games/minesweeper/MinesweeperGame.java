bd9436859cc41a16835789cb6877562f77a11cdf
LanaSpector <mansurova.s.k@gmail.com>
2023-07-09 14:09:06 +0500

    Add solution for MinesweeperGame, part 2 of 16

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -3,5 +3,11 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
+    private static final int SIDE = 9;
+
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
+
 }
 
