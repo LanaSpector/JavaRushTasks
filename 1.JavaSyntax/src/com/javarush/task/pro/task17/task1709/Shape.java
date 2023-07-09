fd063610e3c1030c998a9fd406a1c1ef1e048f5a
LanaSpector <mansurova.s.k@gmail.com>
2023-07-09 11:19:11 +0500

    Add solution for java syntax task1709

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1709/Shape.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task17.task1709;
+
+/* 
+Абстрактные фигуры
+*/
+
+abstract public class Shape {
+    
+    abstract public double getPerimeter();
+    abstract public double getArea();
+    
+}
