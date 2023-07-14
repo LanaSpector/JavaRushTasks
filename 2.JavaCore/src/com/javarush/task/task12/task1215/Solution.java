2187a37f846f00f44a95aa43a7bae2bb461e14da
LanaSpector <mansurova.s.k@gmail.com>
2023-07-14 23:35:32 +0500

    Add solution for JavaCore/task1215

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1215/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task12.task1215;
+
+/* 
+Кошки не должны быть абстрактными!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public abstract String getName();
+
+        public abstract Pet getChild();
+    }
+
+    public static class Cat extends Pet{
+        public String getName() {
+            return "Имя";
+        }
+        public Pet getChild() {
+        return new Cat();
+        }
+    }
+
+    public static class Dog extends Pet{
+        public String getName() {
+            return "Имя";
+        }
+        public Pet getChild() {
+        return new Dog();
+        }
+
+    }
+
+}
