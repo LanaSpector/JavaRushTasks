42ceb37c3cff30e6dd6da9a1a4d051060ff1b1c7
LanaSpector <mansurova.s.k@gmail.com>
2023-07-14 23:31:01 +0500

    Add solution for JavaCore/task1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String q) {
+        
+    }
+    public static void print(int y) {
+        
+    }
+    public static void print(double s) {
+        
+    }
+    public static void print(String t, int e) {
+        
+    }
+    public static void print(float r) {
+        
+    }
+}
