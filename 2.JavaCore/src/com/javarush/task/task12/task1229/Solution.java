2e6fbb139c11c30aa24f5b6b03578aa6b9a7f514
LanaSpector <mansurova.s.k@gmail.com>
2023-07-15 11:36:08 +0500

    Add solution for JavaCore/task1229

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1229/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task12.task1229;
+
+/* 
+Родитель класса CTO
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+    public static class CTO extends Example implements Businessman{
+
+    }
+
+    public static class Example implements Businessman{
+        @Override
+        public void workHard() {
+
+        }
+    }
+}
