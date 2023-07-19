e9c6b08502c162dcdcf6f04c3162e17517f96e63
LanaSpector <mansurova.s.k@gmail.com>
2023-07-19 19:27:08 +0500

    Add solution for JavaCore/task1319

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task13.task1319;
+
+import java.io.*;
+
+/* 
+Писатель в файл с консоли
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(reader.readLine()))) {
+            while (true) {
+                String string = reader.readLine();
+                writer.write(string);
+                writer.newLine();
+                if (string.equalsIgnoreCase("exit")) {
+                    break;
+                }
+            }
+        }
+
+    }
+}
