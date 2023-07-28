781e5a97dd01e8ef355e0ddae669c96c03c33e47
LanaSpector <mansurova.s.k@gmail.com>
2023-07-28 11:59:20 +0500

    Add solution for JavaCore/task1417

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1417/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task14.task1417;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Валюты
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Person ivan = new Person("Иван");
+        for (Money money : ivan.getAllMoney()) {
+            System.out.println(ivan.name + " имеет заначку в размере " + money.getAmount() + " " + money.getCurrencyName());
+        }
+    }
+
+    static class Person {
+        public String name;
+
+        Person(String name) {
+            this.name = name;
+            this.allMoney = new ArrayList<Money>();
+            allMoney.add(new Hryvnia(20));
+            allMoney.add(new Ruble(20));
+            allMoney.add(new USD(17));
+        }
+
+        private List<Money> allMoney;
+
+        public List<Money> getAllMoney() {
+            return allMoney;
+        }
+
+        public void setAllMoney(List<Money> allMoney) {
+
+        }
+    }
+}
