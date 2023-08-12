bda8bbe312a483cdee79d5ca15e9fab1c3052caa
LanaSpector <mansurova.s.k@gmail.com>
2023-08-12 12:24:04 +0500

    Add solution for JavaCore/task1529

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1515/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task15.task1515;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Статики-2
+*/
+
+public class Solution {
+    public static int A;
+    public static int B;
+    static {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));){
+            A = Integer.parseInt(reader.readLine());
+            B = Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static final int MIN = min(A, B);
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MIN);
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements CanFly{
+    @Override
+    public void fly() {}
+
+    public Plane(int quantity) {
+    }
+}
