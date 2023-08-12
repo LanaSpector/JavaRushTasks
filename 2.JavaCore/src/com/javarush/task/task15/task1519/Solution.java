9c5bee44d50ebc14519decc89256fe70706fb5be
LanaSpector <mansurova.s.k@gmail.com>
2023-08-12 12:55:45 +0500

    Add solution for JavaCore/task1519

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1519/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task15.task1519;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Разные методы для разных типов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));) {
+            while (true) {
+                String string = reader.readLine();
+                if (string.equals("exit")) {
+                    break;
+                }
+                try {
+                    if (string.contains(".")) {
+                        print(Double.parseDouble(string));
+                    } else {
+                        int number = Integer.parseInt(string);
+                        if (number > 0 && number < 128) {
+                            print((short) number);
+                        } else {
+                            print(number);
+                        }
+                    }
+                } catch (Exception e) {
+                    print(string);
+                }
+            }
+        }
+    }
+
+    public static void print(Double value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
+
+    public static void print(String value) {
+        System.out.println("Это тип String, значение " + value);
+    }
+
+    public static void print(short value) {
+        System.out.println("Это тип short, значение " + value);
+    }
+
+    public static void print(Integer value) {
+        System.out.println("Это тип Integer, значение " + value);
+    }
+}
