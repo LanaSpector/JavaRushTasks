ed480ae5b478cd8d4ff58eb758b76d5a34bba017
LanaSpector <mansurova.s.k@gmail.com>
2023-08-12 11:44:28 +0500

    Add solution for JavaCore/task1516

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1516/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task15.task1516;
+
+/* 
+Значения по умолчанию
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.intVar);
+        System.out.println(s.doubleVar);
+        System.out.println(s.DoubleVar);
+        System.out.println(s.booleanVar);
+        System.out.println(s.ObjectVar);
+        System.out.println(s.ExceptionVar);
+        System.out.println(s.StringVar);
+    }
+
+    public int intVar;
+    public double doubleVar;
+    public Double DoubleVar;
+    public boolean booleanVar;
+    public Object ObjectVar;
+    public Exception ExceptionVar;
+    public String StringVar;
+    
+}
