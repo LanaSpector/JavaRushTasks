60e20cbb4808717ae1a1a6eb59f7cef57036d449
LanaSpector <mansurova.s.k@gmail.com>
2023-08-19 16:29:53 +0500

    Add solution for JavaCore/task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public Solution(double d) {
+    }
+    private Solution(int e) {
+    }
+    protected Solution(short g) {
+    }
+    Solution() {
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+    public SubSolution(double d) {
+        super(d);
+    }
+
+    public SubSolution(short g) {
+        super(g);
+    }
+
+    public SubSolution() {
+    }
+}
