6e12b157308b15105d5fbcb0743b9503fb08bcca
LanaSpector <mansurova.s.k@gmail.com>
2023-09-02 13:23:14 +0500

    Add solution for JavaCore/task1803

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task18.task1803;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/* 
+Самые частые байты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] byteCount = new int[256];
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream stream = new FileInputStream(reader.readLine());) {
+            while (stream.available() > 0) {
+                byteCount[stream.read()]++;
+            }
+        }
+//        int maxCount = byteCount[0];
+//        for (int i = 0; i < byteCount.length; i++) {
+//            if (byteCount[i] > maxCount) {
+//                maxCount = byteCount[i];
+//            }
+//        }
+//        List<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < byteCount.length; i++) {
+//            if (byteCount[i] == maxCount) {
+//                list.add(i);
+//            }
+//        }
+//        for (Integer integer : list) {
+//            System.out.print(integer + " ");
+//        }
+        IntStream.range(0, byteCount.length)
+                .filter(i -> byteCount[i] == Arrays.stream(byteCount).max().getAsInt())
+                .boxed()
+                .toList()
+                .forEach(i -> System.out.print(i + " "));
+    }
+}
