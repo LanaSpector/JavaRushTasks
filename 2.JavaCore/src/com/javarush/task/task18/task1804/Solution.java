4a886ee9a40bc26e6f811741bc70b1260836d7c1
LanaSpector <mansurova.s.k@gmail.com>
2023-09-03 22:52:16 +0500

    Add solution for JavaCore/task1804

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 /* 
 Самые редкие байты
@@ -15,24 +16,23 @@ public class Solution {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              FileInputStream stream = new FileInputStream(reader.readLine())) {
             while (stream.available() > 0) {
-                for (int i = 0; i < arrayCount.length; i++) {
-                    arrayCount[stream.read()]++;
-                }
+                arrayCount[stream.read()]++;
             }
-            int min = Integer.MAX_VALUE;
-            for (int i = 0; i < arrayCount.length; i++) {
-                if (arrayCount[i]>0 && arrayCount[i] < min) {
-                    min = arrayCount[i];
-                }
-            }
-            for (int i = 0; i < arrayCount.length; i++) {
-                if (arrayCount[i]>0 && arrayCount[i] == min) {
-                    System.out.print(arrayCount[i] + " ");
-                }
-            }
-
         }
-
-
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arrayCount.length; i++) {
+            if (arrayCount[i] > 0 && arrayCount[i] < min) {
+                min = arrayCount[i];
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arrayCount.length; i++) {
+            if (arrayCount[i] == min) {
+                list.add(i);
+            }
+        }
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
     }
 }
