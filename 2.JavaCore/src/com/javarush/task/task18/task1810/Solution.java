dab861cc7e46d37c37f18476c8a53538e5406297
LanaSpector <mansurova.s.k@gmail.com>
2023-09-05 09:17:38 +0500

    Add solution for JavaCore/task1810

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task18.task1810;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+DownloadException
+*/
+
+public class Solution {
+    public static void main(String[] args) throws DownloadException, IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String line = reader.readLine();
+            try (FileInputStream stream = new FileInputStream(line)) {
+                if (stream.available() < 1000) {
+                    throw new DownloadException();
+                }
+            }
+        }
+    }
+
+    public static class DownloadException extends Exception {
+
+
+    }
+}
