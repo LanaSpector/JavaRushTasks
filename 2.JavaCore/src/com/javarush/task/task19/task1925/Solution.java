ebc764e04641f4525b4fdc92138bf151b13a6b88
LanaSpector <mansurova.s.k@gmail.com>
2023-09-27 10:05:35 +0500

    Add solution for JavaCore/task1925

--- 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -12,17 +12,19 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+        StringBuilder string = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(args[0]));
              FileWriter writer = new FileWriter(args[1])) {
             while (reader.ready()) {
                 String[] strings = reader.readLine().split(" ");
-                for (String string : strings) {
-                    if (string.length() > 6) {
-                        writer.write(string + ",");
+                for (String line : strings) {
+                    if (line.length() > 6) {
+                        string.append(line).append(" ");
                     }
                 }
+                String str = string.toString().trim().replaceAll(" ", ",");
+                writer.write(str);
             }
-
         }
 
     }
