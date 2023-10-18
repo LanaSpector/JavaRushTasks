2f14b080508053c7b0fc7ea87fd182c9d3b95d46
LanaSpector <mansurova.s.k@gmail.com>
2023-10-18 21:42:04 +0500

    Add solution for SQL/task0327

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0327/query.sql
@@ -0,0 +1,5 @@
+select customers.city, orders.store_id
+from customers
+         right join orders on customers.customer_id = orders.customer_id
+where orders.order_status = 'SHIPPED'
+  and orders.total_cost > 100;
