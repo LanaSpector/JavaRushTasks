4b0ad09a3965021ab67ad9b650c3377475b4c0ff
LanaSpector <mansurova.s.k@gmail.com>
2023-10-18 21:40:58 +0500

    Add solution for SQL/task0318

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0318/query.sql
@@ -0,0 +1,3 @@
+select top_retailers.ret_name, top_retailers.ret_revenue, suppliers.sup_name, suppliers.sup_revenue
+from top_retailers
+         join suppliers on (top_retailers.ret_revenue = suppliers.sup_revenue);
