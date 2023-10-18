9031c8ff100b6fffd9b2aa0133459ba662d75dd6
LanaSpector <mansurova.s.k@gmail.com>
2023-10-18 21:41:29 +0500

    Add solution for SQL/task0322

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0322/query.sql
@@ -0,0 +1,5 @@
+select c.customer_id, c.email, o.order_id, o.order_status
+from customers as c
+         join orders as o on (c.customer_id = o.order_id)
+where o.order_status = 'PAID'
+   or o.order_status = 'PROCESSING';
