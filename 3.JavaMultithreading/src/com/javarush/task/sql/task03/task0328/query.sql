8e9f7d147239129901f73663c1ecc2bb4bdc1dbb
LanaSpector <mansurova.s.k@gmail.com>
2023-10-19 21:53:11 +0500

    Add solution for SQL/task0328

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0328/query.sql
@@ -0,0 +1,5 @@
+select customers.phone as cust_phone, orders.order_date, orders.total_cost, orders.store_id
+from customers
+         right join orders on customers.customer_id = orders.customer_id
+where customers.city = 'New York'
+  and orders.order_status = 'PROCESSING';
