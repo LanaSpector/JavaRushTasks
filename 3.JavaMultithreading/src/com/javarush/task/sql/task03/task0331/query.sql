810c6ab44fa98b793d9315e811f5bbdf41ed48ed
LanaSpector <mansurova.s.k@gmail.com>
2023-10-19 21:53:48 +0500

    Add solution for SQL/task0331

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0331/query.sql
@@ -0,0 +1,4 @@
+select a.last_name as author, b.title as book_title, b.copies_sold_millions as books_sold
+from authors as a
+         right join books as b on a.author_id = b.author_id
+where books_sold > 50;
