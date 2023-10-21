ccaa80f346a060cde7c4963f8b1991573a6c0452
LanaSpector <mansurova.s.k@gmail.com>
2023-10-21 11:42:59 +0500

    Add solution for SQL/task0346

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task03/task0346/query.sql
@@ -0,0 +1,5 @@
+select *
+from films
+         join (select year_born, id from film_directors where year_born < 1940) as director
+              on director.id = films.director_id
+where films.genre = 'sci-fi';
