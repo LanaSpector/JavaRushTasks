688f7dc4ec1f4b8498ce84efc3825bc1aba4c2c4
LanaSpector <mansurova.s.k@gmail.com>
2023-10-28 13:52:03 +0500

    Add solution for SQL/task0505

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0505/query.sql
@@ -0,0 +1,5 @@
+INSERT INTO object (name, type, discoverer, discovery_date) VALUES
+                                                                ('2020 XL5', 'ASTEROID', 'Pan-STARRS', '2020-12-12'),
+                                                                ('Moon', 'SATELLITE', null, null),
+                                                                ('Jupiter', 'PLANET', null, null),
+                                                                ('C/1910 A1', 'COMET', 'Robert T. A. Innes', '1910-01-17');
