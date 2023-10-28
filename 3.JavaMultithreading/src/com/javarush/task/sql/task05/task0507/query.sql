12d52f8daf51e33902262614ba642dad80c762a0
LanaSpector <mansurova.s.k@gmail.com>
2023-10-28 13:52:15 +0500

    Add solution for SQL/task0507

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task05/task0507/query.sql
@@ -0,0 +1,5 @@
+UPDATE part SET name = 'Technic, Steering Wheel Hub 3 Pin Round'
+WHERE number = '92909';
+
+UPDATE part SET name = 'Minifigure, Headgear Helmet SW Darth Vader Type 2 Top'
+WHERE number = '19916';
