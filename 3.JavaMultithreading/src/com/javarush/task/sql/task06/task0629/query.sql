75cf11e7a42832aee79c246134c8b6a3faeb4029
LanaSpector <mansurova.s.k@gmail.com>
2023-11-07 17:18:09 +0500

    Add solution for SQL/task0629

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task06/task0629/query.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `event`
+(
+    `id`        INT         not null    auto_increment,
+    `date`      DATE        not null,
+    `user_id`   INT         not null,
+    `type`      VARCHAR(20) not null,
+    `status`    VARCHAR(10) not null,
+    PRIMARY KEY(id),
+    foreign key (user_id) references user (id)
+);
+
+insert into event (id, date, user_id, type, status)
+values (1, '2022-10-01', 1, 'REGISTRATION', 'OK');
