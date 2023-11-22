1899ad2f605b3212775dd7e1bebe21a4eba09f78
LanaSpector <mansurova.s.k@gmail.com>
2023-11-22 20:05:59 +0500

    Add solution for JavaCore/task3509

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task35/task3509/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.jdk13.task35.task3509;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
+/* 
+Wildcards для коллекций
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static Double sum(List<? extends Number> list) {
+//        double result = 0.0;
+//        for (int i = 0; i < list.size(); i++) {
+//            Number numb = list.get(i);
+//            result += numb.doubleValue();
+//        }
+//        return result;
+        return list.stream().mapToDouble(Number::doubleValue).sum();
+    }
+
+    public static Double multiply(List<? extends Number> list) {
+//        double result = 1.0;
+//        for (Number numb : list) {
+//            result *= numb.doubleValue();
+//        }
+//        return result;
+        return list.stream().mapToDouble(Number::doubleValue).reduce(1.0, (a, b) -> a * b);
+    }
+
+    public static String concat(List<?> list) {
+//        StringBuilder builder = new StringBuilder();
+//        for (Object obj : list) {
+//            builder.append(obj);
+//        }
+//        return builder.toString();
+        return list.stream().map(String::valueOf).collect(Collectors.joining());
+    }
+
+    public static <T> List<T> combine(List<? extends Collection<T>> list) {
+//        List<T> result = new ArrayList<>();
+//        for (Collection<T> collection : list) {
+//            result.addAll(collection);
+//        }
+//        return result;
+        return list.stream().flatMap(Collection::stream).collect(Collectors.toList());
+    }
+}
