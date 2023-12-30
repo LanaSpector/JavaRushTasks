24b5b2eb87ded950a3cacbd73b8bc1095d03f949
LanaSpector <mansurova.s.k@gmail.com>
2023-12-30 13:30:48 +0500

    Add solution for JavaCore/task1916

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task19.task1916;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +11,41 @@ import java.util.List;
 public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = console.readLine();
+        String file2 = console.readLine();
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file1));
+             BufferedReader reader1 = new BufferedReader(new FileReader(file2))) {
+            while (reader.ready()) {
+                list1.add(reader.readLine());
+            }
+            while (reader1.ready()) {
+                list2.add(reader1.readLine());
+            }
+        }
+        while (!list1.isEmpty() && !list2.isEmpty()) {
+            if (list1.get(0).equals(list2.get(0))) {
+                lines.add(new LineItem(Type.SAME, list1.get(0)));
+                list1.remove(0);
+                list2.remove(0);
+            } else if (list1.get(0).equals(list2.get(1))) {
+                lines.add(new LineItem(Type.ADDED, list2.get(0)));
+                list2.remove(0);
+            } else if (list1.get(1).equals(list2.get(0))) {
+                lines.add(new LineItem(Type.REMOVED, list1.get(0)));
+                list1.remove(0);
+            }
+        }
+        if (list1.isEmpty()) {
+            lines.add(new LineItem(Type.ADDED, list2.get(0)));
+        }
+        if (list2.isEmpty()) {
+            lines.add(new LineItem(Type.REMOVED,list1.get(0)));
+        }
+        console.close();
     }
 
 
