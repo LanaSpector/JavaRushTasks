7f6279e956e965c0739d113d6f0919f538b0439a
LanaSpector <mansurova.s.k@gmail.com>
2024-01-09 23:27:11 +0500

    Add solution for JavaMultithreading/task2113(3)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,9 +1,14 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
-    private List<Horse> horses;
+    private List<Horse> horses = new ArrayList<>();
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
 
     public List<Horse> getHorses() {
         return horses;
