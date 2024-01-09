59020a44882dea41097fceb71480ac105d1de992
LanaSpector <mansurova.s.k@gmail.com>
2024-01-09 23:42:23 +0500

    Add solution for JavaMultithreading/task2113(6)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
-    private List<Horse> horses = new ArrayList<>();
+    private List<Horse> horses;
+    static Hippodrome game;
 
     public Hippodrome(List<Horse> horses) {
         this.horses = horses;
@@ -15,6 +16,17 @@ public class Hippodrome {
     }
 
     public static void main(String[] args) {
-        
+        List<Horse> horses = new ArrayList<>();
+        Horse whiteHorse = new Horse("White", 3, 0);
+        Horse blueHorse = new Horse("Blue", 3, 0);
+        Horse redHorse = new Horse("Red", 3, 0);
+
+        horses.add(whiteHorse);
+        horses.add(blueHorse);
+        horses.add(redHorse);
+
+        game = new Hippodrome(horses);
+
+
     }
 }
