2f1444414138490117a0a1aeba35346ad3faa879
LanaSpector <mansurova.s.k@gmail.com>
2024-01-13 13:43:53 +0500

    Add solution for JavaMultithreading/task2209

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task22.task2209;
+
+import java.io.*;
+import java.util.*;
+
+/* 
+Составить цепочку слов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String[] strings = null;
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader reader = new BufferedReader(new FileReader(console.readLine()))) {
+            while (reader.ready()) {
+                strings = reader.readLine().split(" ");
+            }
+        }
+
+        StringBuilder result = getLine(strings);
+        System.out.println(result.toString());
+    }
+
+    public static StringBuilder getLine(String... words) {
+        if (words == null || words.length == 0) {
+            return new StringBuilder();
+        }
+        StringBuilder builder = new StringBuilder();
+        List<StringBuilder> builderList = new ArrayList<>();
+        for (String word : words) {
+            builder.append(word);
+
+            List<String> list = new ArrayList<>(Arrays.asList(words));
+            list.remove(word);
+
+            int count = 0;
+            while (!list.isEmpty()) {
+                String firstStartChar = builder.substring(0, 1);
+                String firstEndChar = builder.substring(builder.length() - 1);
+
+                String secondStartChar = list.get(0).substring(0, 1);
+                String secondEndChar = list.get(0).substring(list.get(0).length() - 1);
+
+                if (firstEndChar.equalsIgnoreCase(secondStartChar)) {
+                    builder.append(" ").append(list.get(0));
+                    list.remove(0);
+
+                } else if (firstStartChar.equalsIgnoreCase(secondEndChar)) {
+                    builder.insert(0, list.get(0) + " ");
+                    list.remove(0);
+
+                } else {
+                    String string = list.get(0);
+                    list.remove(0);
+                    list.add(string);
+                    count++;
+                    if (count > Math.pow(words.length, 2)) {
+                        break;
+                    }
+                }
+            }
+            builderList.add(builder);
+            builder = new StringBuilder();
+        }
+        return builderList.stream().max(Comparator.comparingInt(StringBuilder::length)).get();
+    }
+}
