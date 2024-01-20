deed6f07330507ee2489d4f227cd0a28c248725f
LanaSpector <mansurova.s.k@gmail.com>
2024-01-20 23:26:14 +0500

    Add solution for JavaMultithreading/task2203

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task22.task2203;
+
+/* 
+Между табуляциями
+*/
+
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string == null) {
+            throw new TooShortStringException();
+        }
+        String[] splitedString = string.split("\t");
+        if (splitedString.length < 3) {
+            throw new TooShortStringException();
+        }
+        return splitedString[1];
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
+    }
+}
