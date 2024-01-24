04db3bfac1e1022c90b90d7b559f46d5e789d4f0
LanaSpector <mansurova.s.k@gmail.com>
2024-01-24 18:51:01 +0500

    Add solution for JavaMultithreading/task2909(3)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Human {
+public class Human implements Alive {
     public static int nextId = 0;
     private int id;
     protected int age;
     protected String name;
-    protected int course;
     private List<Human> children = new ArrayList<>();
 
     protected int[] size;
@@ -64,10 +63,7 @@ public class Human {
         this.name = name;
     }
 
-    public int getCourse() {
-        return course;
-    }
-
+    @Override
     public void live() {
 
     }
