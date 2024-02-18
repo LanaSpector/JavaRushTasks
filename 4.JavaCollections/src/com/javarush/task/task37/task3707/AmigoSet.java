113695bcf4b508ae52cf44a438308e0bcf4a5a4f
LanaSpector <mansurova.s.k@gmail.com>
2024-02-18 12:26:08 +0500

    Add solution for JavaCollections/task3707(2)

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,13 +1,23 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        this.map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        map = new HashMap<>(Math.max(16, (int) (collection.size() / .75f) + 1));
+        this.addAll(collection);
+    }
 
     @Override
+
     public Iterator<E> iterator() {
         return null;
     }
@@ -15,5 +25,11 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
     @Override
     public int size() {
         return 0;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+
     }
 }
