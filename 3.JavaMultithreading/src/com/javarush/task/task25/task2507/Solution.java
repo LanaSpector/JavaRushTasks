c19a911693e66ed359b3770f56e9e49280b0a3f1
LanaSpector <mansurova.s.k@gmail.com>
2024-02-28 18:30:02 +0500

    Add solution for JavaMultithreading/task2507

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2507/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2507/Solution.java
@@ -19,7 +19,14 @@ public class Solution extends Thread {
     }
 
     public void interrupt() {
-        //implement logic here
+        try {
+            socket.close();
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            super.interrupt();
+        }
     }
 
     public void run() {
