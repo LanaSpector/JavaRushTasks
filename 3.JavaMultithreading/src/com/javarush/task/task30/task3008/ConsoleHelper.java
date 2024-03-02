3b02ec1bb07b94ea7b6f03c036a49fb98f8a43c4
LanaSpector <mansurova.s.k@gmail.com>
2024-03-02 12:32:38 +0500

    Add solution for JavaMultithreading/task2609

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,36 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String string;
+        try {
+            string = console.readLine();
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            string = readString();
+
+        }
+        return string;
+    }
+
+    public static int readInt() {
+        int number;
+        try {
+            number = Integer.parseInt(readString());
+        } catch (NumberFormatException e) {
+            System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            number = readInt();
+        }
+        return  number;
+    }
+
 }
--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -2,6 +2,6 @@ package com.javarush.task.task30.task3008;
 
 public class Server {
     public static void main(String[] args) {
-
+        
     }
 }
