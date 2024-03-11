c73d5ce5ee2c32bfdd23a226429a575034ee9b5f
LanaSpector <mansurova.s.k@gmail.com>
2024-03-11 22:57:03 +0500

    Add solution for JavaMultithreading/task 2612

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2612/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task26.task2612;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/* 
+Весь мир играет комедию
+*/
+
+public class Solution {
+    private Lock lock = new ReentrantLock();
+
+    public void someMethod() throws InterruptedException {
+        if (lock.tryLock()) {
+            try {
+                actionIfLockIsFree();
+            } catch (Exception e) {
+                lock.unlock();
+            }
+        } else {
+            actionIfLockIsBusy();
+        }
+    }
+
+    public void actionIfLockIsFree() {
+    }
+
+    public void actionIfLockIsBusy() {
+    }
+}
