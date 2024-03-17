564492871fa5b708c0d8f2feafecd992f5f513ed
LanaSpector <mansurova.s.k@gmail.com>
2024-03-17 23:26:03 +0500

    Add solution for JavaMultithreading/task 2610

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,12 +1,24 @@
-package com.javarush.task.task30.task3008.client;
-
-import com.javarush.task.task30.task3008.Connection;
-
-public class Client {
-    protected Connection connection;
-    private volatile boolean clientConnected = false;
-
-    public class SocketThread extends Thread {
-
-    }
-}
+//package com.javarush.task.task30.task3008.client;
+//
+//import com.javarush.task.task30.task3008.Connection;
+//
+//public class Client {
+//    protected Connection connection;
+//    private volatile boolean clientConnected = false;
+//
+//    protected String getServerAddress() {return null;}
+//
+//    protected int getServerPort(){return 0;}
+//
+//    protected String getUserName(){return null;}
+//
+//    protected boolean shouldSendTextFromConsole() {return false;}
+//
+//    protected SocketThread getSocketThread(){return }
+//
+//    protected void sendTextMessage(String text){}
+//
+//
+//    public class SocketThread extends Thread {
+//    }
+//}
