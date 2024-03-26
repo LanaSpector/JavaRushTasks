c22c2e064c1f8bbd251f818aa62d7e43068edf49
LanaSpector <mansurova.s.k@gmail.com>
2024-03-26 22:17:22 +0500

    Add solution for JavaMultithreading/task 2413(20)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,8 +1,13 @@
 package com.javarush.task.task24.task2413;
 
+/**
+ * Класс-холст для отрисовки.
+ */
 public class Canvas {
+    //ширина и высота
     private int width;
     private int height;
+    //матрица, где рисуем. символ - это цвет.
     private char[][] matrix;
 
     public Canvas(int width, int height) {
@@ -11,28 +16,11 @@ public class Canvas {
         this.matrix = new char[height + 2][width + 2];
     }
 
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public char[][] getMatrix() {
-        return matrix;
-    }
-
-    public void setMatrix(char[][] matrix) {
-        this.matrix = matrix;
+    /**
+     * Очищаем холст
+     */
+    void clear() {
+        this.matrix = new char[height + 2][width + 2];
     }
 
     /**
@@ -45,7 +33,7 @@ public class Canvas {
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (matrix[i][j] != 0)
+                if (matrix[i][j] == 1)
                     setPoint(x + j, y + i, c);
             }
         }
@@ -62,17 +50,37 @@ public class Canvas {
 
         matrix[y0][x0] = c;
     }
-    
-    public void clear(){
-        matrix = new char[height+2][width+2];
-    }
-    
-    public void print(){
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0; j<matrix[i].length; j++){
+
+    /**
+     * Печатаем содержимое холста на экран.
+     */
+    void print() {
+        System.out.println();
+
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
+                System.out.print(" ");
                 System.out.print(matrix[i][j]);
+                System.out.print(" ");
             }
+
             System.out.println();
         }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
     }
 }
