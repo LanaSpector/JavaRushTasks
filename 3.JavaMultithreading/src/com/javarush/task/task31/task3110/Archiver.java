b5d91c87f7f2a56b1f9ecc956952ba91a47a8744
LanaSpector <mansurova.s.k@gmail.com>
2024-03-27 18:33:00 +0500

    Add solution for JavaMultithreading/task 3110(1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите полный путь к архиву с клавиатуры");
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = console.readLine();
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(fileName));
+        System.out.println("Введите путь к файлу для архивации");
+        String archive = console.readLine();
+        zipFileManager.createZip(Paths.get(archive));
+    }
+}
