d9ffc990b892b3e196c71f8842005029aaef2118
LanaSpector <mansurova.s.k@gmail.com>
2024-03-27 19:17:58 +0500

    Add solution for JavaMultithreading/task 3110(2)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -9,7 +13,20 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
 
-    public void createZip(Path source) throws Exception{
+    public void createZip(Path source) throws Exception {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            try(InputStream inputStream = Files.newInputStream(source)) {
+
+                ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+                zipOutputStream.putNextEntry(zipEntry);
+
+                while (inputStream.available() > 0) {
+                    int read = inputStream.read();
+                    zipOutputStream.write(read);
+                }
+                zipOutputStream.closeEntry();
+            }
+        }
 
     }
 }
