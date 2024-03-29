04162301d69a41838ef16f074011b151e864bbca
LanaSpector <mansurova.s.k@gmail.com>
2024-03-29 23:23:16 +0500

    Add solution for JavaMultithreading/task 2710

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2710/MailServer.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task27.task2710;
+
+public class MailServer implements Runnable {
+    private Mail mail;
+
+    public MailServer(Mail mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public void run() {
+           long startTime = System.currentTimeMillis();
+        try{
+        synchronized(mail) {
+            mail.wait();
+        }
+    
+        //сделайте что-то тут - do something here
+        String name = Thread.currentThread().getName();
+        long endTime = System.currentTimeMillis();
+        System.out.format("%s MailServer received: [%s] in %d ms after start", name, mail.getText(), (endTime - startTime));
+        }catch(InterruptedException ignored){}
+    }
+}
