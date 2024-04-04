7cf23abaf4d292abb34f7c60cc72670bfc2495e7
LanaSpector <mansurova.s.k@gmail.com>
2024-04-04 22:40:15 +0500

    Add solution for JavaMultithreading/task 3110(11)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,7 +1,18 @@
 package com.javarush.task.task31.task3110.command;
 
-public abstract class ZipCommand implements Command{
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public abstract class ZipCommand implements Command {
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Пожалуйста, введите полный путь файла архива");
+        String archiveFilePath = ConsoleHelper.readString();
+        Path pathToArchiveFile = Paths.get(archiveFilePath);
+        return new ZipFileManager(pathToArchiveFile);
+    }
 
 
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
@@ -1,8 +1,26 @@
 package com.javarush.task.task31.task3110.command;
 
-public class ZipCreateCommand extends ZipCommand{
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
+            ZipFileManager zipFileManager = getZipFileManager();
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации");
+            String readString = ConsoleHelper.readString();
+            Path newPath = Paths.get(readString);
+            zipFileManager.createZip(newPath);
+            ConsoleHelper.writeMessage("Архив создан.");
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
 
     }
 }
