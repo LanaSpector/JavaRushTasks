8e229d4fb97e82090a9c700225a73fb5a536254d
LanaSpector <mansurova.s.k@gmail.com>
2024-04-13 12:16:44 +0500

    Add solution for JavaMultithreading/task 2802

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2802/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task28.task2802;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* 
+Пишем свою ThreadFactory
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        class EmulatorThreadFactoryTask implements Runnable {
+            @Override
+            public void run() {
+                emulateThreadFactory();
+            }
+        }
+
+        ThreadGroup group = new ThreadGroup("firstGroup");
+        Thread thread = new Thread(group, new EmulatorThreadFactoryTask());
+
+        ThreadGroup group2 = new ThreadGroup("secondGroup");
+        Thread thread2 = new Thread(group2, new EmulatorThreadFactoryTask());
+
+        thread.start();
+        thread2.start();
+    }
+
+    private static void emulateThreadFactory() {
+        AmigoThreadFactory factory = new AmigoThreadFactory();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+    }
+
+    public static class AmigoThreadFactory implements ThreadFactory {
+        private final String name;
+
+        private static final AtomicInteger FABRIC_NUMBER = new AtomicInteger(1);
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+        public AmigoThreadFactory() {
+            ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
+            name = String.format("%s-pool-%s-thread-", threadGroup.getName(),
+                    FABRIC_NUMBER.getAndIncrement());
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r, name + threadNumber.getAndIncrement());
+            thread.setDaemon(false);
+            thread.setPriority(Thread.NORM_PRIORITY);
+            return thread;
+        }
+    }
+}
