11a8e33235c57adc5b0d0432462936c7e051feb5
LanaSpector <mansurova.s.k@gmail.com>
2024-04-22 00:04:28 +0500

    Add solution for JavaMultithreading/task 3209(2)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -17,6 +17,12 @@ public class View extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        
+    }
 
+    public void init(){}
+
+    public void exit() {
+        controller.exit();
     }
 }
