9b5dcb23ad7330136cfcbae28ef3f7b4f3f73792
LanaSpector <mansurova.s.k@gmail.com>
2024-04-24 15:13:24 +0500

    Add solution for JavaMultithreading/task 3209(6)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -36,15 +38,27 @@ public class View extends JFrame implements ActionListener {
         controller.exit();
     }
 
-    public void initMenuBar(){}
+    public void initMenuBar() {
+    }
 
-    public void initEditor(){}
+    public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane pane = new JScrollPane(htmlTextPane);
+        tabbedPane.add("HTML", pane);
+        JScrollPane jScrollPane = new JScrollPane(plainTextPane);
+        tabbedPane.add("Текст", jScrollPane);
+        tabbedPane.setPreferredSize(null);
+        TabbedPaneChangeListener listener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(listener);
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
 
-    public void initGui(){
+    public void initGui() {
         initMenuBar();
         initEditor();
         pack();
     }
 
-    public void selectedTabChanged(){}
+    public void selectedTabChanged() {
+    }
 }
