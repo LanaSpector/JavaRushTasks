daca641ff2839bec4879ec62b3932867fc1434d1
LanaSpector <mansurova.s.k@gmail.com>
2024-05-15 22:25:05 +0500

    Add solution for JavaMultithreading/task 2712(2)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -4,5 +4,10 @@ import com.javarush.task.task27.task2712.kitchen.Dish;
 
 public class Restaurant {
     public static void main(String[] args) {
+        Tablet tablet = new Tablet(1);
+        tablet.createOrder();
+        tablet.createOrder();
+        tablet.createOrder();
+        tablet.createOrder();
     }
 }
