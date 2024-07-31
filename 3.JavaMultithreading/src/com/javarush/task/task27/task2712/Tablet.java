789593ec2c89c3321f9e8fe6cfc9ed7a34c0531f
LanaSpector <mansurova.s.k@gmail.com>
2024-07-31 22:05:58 +0500

    Add solution for JavaMultithreading/task 2712(9)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.ad.AdvertisementManager;
+import com.javarush.task.task27.task2712.ad.NoVideoAvailableException;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
@@ -26,7 +27,11 @@ public class Tablet extends Observable {
                 notifyObservers(order);
             }
             AdvertisementManager manager = new AdvertisementManager(order.getTotalCookingTime() * 60);
-            manager.processVideos();
+            try {
+                manager.processVideos();
+            } catch (NoVideoAvailableException e) {
+                logger.log(Level.INFO, "No video is available for the order " + order);
+            }
             return order;
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Console is unavailable.");
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -2,6 +2,9 @@ package com.javarush.task.task27.task2712.ad;
 
 import com.javarush.task.task27.task2712.ConsoleHelper;
 
+import java.util.Collections;
+import java.util.Comparator;
+
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
     private int timeSeconds;
@@ -11,6 +14,26 @@ public class AdvertisementManager {
     }
 
     public void processVideos() {
-        ConsoleHelper.writeMessage("calling processVideos method");
+        if (storage.list().isEmpty()) {
+            throw new NoVideoAvailableException();
+        }
+//        Collections.sort(storage.list(), new Comparator<Advertisement>() {
+//            @Override
+//            public int compare(Advertisement o1, Advertisement o2) {
+//                if (o1.getAmountPerOneDisplaying() > o2.getAmountPerOneDisplaying()) {
+//                    return -1;
+//                }
+//                if (o1.getAmountPerOneDisplaying() < o2.getAmountPerOneDisplaying()) {
+//                    return 1;
+//                }
+//                if (o1.getAmountPerOneDisplaying() / o1.getDuration() > o2.getAmountPerOneDisplaying() / o2.getDuration()) {
+//                    return 1;
+//                }
+//                if (o1.getAmountPerOneDisplaying() / o1.getDuration() < o2.getAmountPerOneDisplaying() / o2.getDuration()) {
+//                    return -1;
+//                }
+//                return 0;
+//            }
+//        });
     }
 }
