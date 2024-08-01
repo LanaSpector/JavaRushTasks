2f080e5b50d35371abd3168de6d2c39947d85f71
LanaSpector <mansurova.s.k@gmail.com>
2024-08-01 17:58:02 +0500

    Add solution for JavaMultithreading/task 3209(16)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.text.BadLocationException;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 
 public class Controller {
     private View view;
@@ -35,11 +38,21 @@ public class Controller {
 
     public void resetDocument() {
         if (document != null) {
-           document.removeUndoableEditListener(view.getUndoListener());
+            document.removeUndoableEditListener(view.getUndoListener());
         }
         HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
         document = (HTMLDocument) htmlEditorKit.createDefaultDocument();
         document.addUndoableEditListener(view.getUndoListener());
         view.update();
+    }
+
+    public void setPlainText(String text) {
+        resetDocument();
+        try (StringReader reader = new StringReader(text)) {
+            HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+            htmlEditorKit.read(reader, document, 0);
+        } catch (IOException | BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
     }
 }
