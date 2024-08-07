c6d05faa83b0f54ed858848ee91b8efcdd0eccc7
LanaSpector <mansurova.s.k@gmail.com>
2024-08-07 15:57:15 +0500

    Add solution for JavaCollections/task 3109

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task31.task3109;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+/* 
+Читаем конфиги
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Properties properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.xml");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.txt");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/notExists");
+        properties.list(System.out);
+    }
+
+    public Properties getProperties(String fileName) {
+        Properties properties = new Properties();
+        try (InputStream input = new FileInputStream(fileName)) {
+            if(fileName.endsWith("xml")) {
+             properties.loadFromXML(input);
+            } else {
+                properties.load(input);
+            }
+        } catch(IOException e) {
+            
+        }
+        return properties;
+    }
+}
