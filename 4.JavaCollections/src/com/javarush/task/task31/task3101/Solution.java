28529f9b325dfed0394871810abd823023d9b84b
LanaSpector <mansurova.s.k@gmail.com>
2024-08-08 18:56:41 +0500

    Add solution for JavaCollections/task 3101

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,20 +1,46 @@
 package com.javarush.task.task31.task3101;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.EnumSet;
-import java.util.Map;
-import java.util.TreeMap;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.*;
 
 /* 
 Проход по дереву файлов
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File sourceDirectory = new File(args[0]);
+        File resultFile = new File(args[1]);
+        File allFilesContent = new File(resultFile.getParent() + "/allFilesContent.txt");
+        if (FileUtils.isExist(allFilesContent)) {
+            FileUtils.deleteFile(allFilesContent);
+        }
+        FileUtils.renameFile(resultFile, allFilesContent);
+        List<File> list = new ArrayList<>();
+        fillListWithFiles(sourceDirectory, list);
+        try (FileOutputStream out = new FileOutputStream(allFilesContent, true)) {
+            for (File file : list) {
+                if (file.length() <= 50) {
+                    byte[] bytes = Files.readAllBytes(file.toPath());
+                    out.write(bytes);
+                    out.write(System.lineSeparator().getBytes());
+                }
+            }
+        }
+    }
 
+    private static void fillListWithFiles(File directory, List<File> list) {
+        File[] files = directory.listFiles();
+        if (files == null) {
+            return;
+        }
+        for (File smth : files) {
+            if (smth.isDirectory()) {
+                fillListWithFiles(smth, list);
+            } else {
+                list.add(smth);
+            }
+        }
     }
 }
