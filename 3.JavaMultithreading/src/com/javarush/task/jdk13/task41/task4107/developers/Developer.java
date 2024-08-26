cc8eeeb86e4ee89177df373ce0fd1f4069fa1983
LanaSpector <mansurova.s.k@gmail.com>
2024-08-26 20:48:03 +0500

    Add solution for JavaMultithreading ( task 4107

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4107/developers/Developer.java
@@ -0,0 +1,7 @@
+package com.javarush.task.jdk13.task41.task4107.developers;
+
+public interface Developer {
+    public void study();
+
+    public void writeCode();
+}
--- 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4107/developers/KotlinDeveloper.java
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4107/developers/KotlinDeveloper.java
@@ -1,9 +1,9 @@
 package com.javarush.task.jdk13.task41.task4107.developers;
 
-public class KotlinDeveloper extends JavaDeveloper {
+
+public class KotlinDeveloper extends JavaDeveloper implements Developer{
     @Override
     public void study() {
-        super.study();
         System.out.println("Изучает особенности котлина...");
         writeCode();
     }
