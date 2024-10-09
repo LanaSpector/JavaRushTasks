955092713c4638144794e68509472ed9599a4a01
LanaSpector <mansurova.s.k@gmail.com>
2024-10-09 10:41:12 +0500

    Add solution for JavaCollections, task 3303

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3303/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task33.task3303;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+/* 
+Десериализация JSON объекта
+*/
+
+public class Solution {
+    public static <T> T convertFromJsonToNormal(String fileName, Class<T> clazz) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(new File(fileName), clazz);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
