291461b09280e68802064a336b50524ae2c5895e
LanaSpector <mansurova.s.k@gmail.com>
2024-10-10 17:09:25 +0500

    Add solution for JavaCollections, task 3305

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3305/Vehicle.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task33.task3305;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS,include=JsonTypeInfo.As.PROPERTY, property = "className")
+public abstract class Vehicle {
+    protected String name;
+    protected String owner;
+    protected int age;
+}
